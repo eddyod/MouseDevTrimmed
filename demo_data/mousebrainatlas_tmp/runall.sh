829f4d231945e9ce91eb44cf05fe3a9faa6955b2
Edward O'Donnell <eodonnell@ucsd.edu>
2020-01-13 10:44:52 -0800

    Added rescale

--- demo_data/mousebrainatlas_tmp/runall.sh
+++ demo_data/mousebrainatlas_tmp/runall.sh
@@ -1,27 +1,2 @@
-/usr/local/bin/convert -channel B -separate \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F68-2017.06.06-07.39.27_MD661_2_0203_raw.tif" \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw_Ntb/MD662&661-F68-2017.06.06-07.39.27_MD661_2_0203_raw_Ntb.tif"
-
-/usr/local/bin/convert -channel B -separate \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F73-2017.06.06-09.53.20_MD661_1_0217_raw.tif" \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw_Ntb/MD662&661-F73-2017.06.06-09.53.20_MD661_1_0217_raw_Ntb.tif"
-
-/usr/local/bin/convert -channel B -separate \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F79-2017.06.06-11.52.28_MD661_1_0235_raw.tif" \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw_Ntb/MD662&661-F79-2017.06.06-11.52.28_MD661_1_0235_raw_Ntb.tif"
-
-/usr/local/bin/convert -channel B -separate \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F84-2017.06.06-14.03.51_MD661_1_0250_raw.tif" \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw_Ntb/MD662&661-F84-2017.06.06-14.03.51_MD661_1_0250_raw_Ntb.tif"
-
-/usr/local/bin/convert -channel B -separate \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F89-2017.06.06-16.49.49_MD661_1_0265_raw.tif" \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw_Ntb/MD662&661-F89-2017.06.06-16.49.49_MD661_1_0265_raw_Ntb.tif"
-
-/usr/local/bin/convert -channel B -separate \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F94-2017.06.06-19.01.05_MD661_1_0280_raw.tif" \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw_Ntb/MD662&661-F94-2017.06.06-19.01.05_MD661_1_0280_raw_Ntb.tif"
-
-/usr/local/bin/convert -channel B -separate \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F99-2017.06.06-21.14.03_MD661_1_0295_raw.tif" \
-"/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw_Ntb/MD662&661-F99-2017.06.06-21.14.03_MD661_1_0295_raw_Ntb.tif"
+/home/eodonnell/git/MouseBrainAtlas_dev/src/utilities/sequential_dispatcher.py b'\'export LD_LIBRARY_PATH=/usr/local/bin:$LD_LIBRARY_PATH; /usr/local/bin/kdu_expand -i "%(in_fp)s" -o "%(out_fp)s"\'' b'\'[{"in_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/jp2_files/DEMO998/MD662&661-F94-2017.06.06-19.01.05_MD661_1_0280_lossless.jp2", "out_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F94-2017.06.06-19.01.05_MD661_1_0280_raw.tif"}, {"in_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/jp2_files/DEMO998/MD662&661-F79-2017.06.06-11.52.28_MD661_1_0235_lossless.jp2", "out_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F79-2017.06.06-11.52.28_MD661_1_0235_raw.tif"}, {"in_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/jp2_files/DEMO998/MD662&661-F89-2017.06.06-16.49.49_MD661_1_0265_lossless.jp2", "out_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F89-2017.06.06-16.49.49_MD661_1_0265_raw.tif"}, {"in_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/jp2_files/DEMO998/MD662&661-F99-2017.06.06-21.14.03_MD661_1_0295_lossless.jp2", "out_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F99-2017.06.06-21.14.03_MD661_1_0295_raw.tif"}, {"in_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/jp2_files/DEMO998/MD662&661-F84-2017.06.06-14.03.51_MD661_1_0250_lossless.jp2", "out_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F84-2017.06.06-14.03.51_MD661_1_0250_raw.tif"}, {"in_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/jp2_files/DEMO998/MD662&661-F73-2017.06.06-09.53.20_MD661_1_0217_lossless.jp2", "out_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F73-2017.06.06-09.53.20_MD661_1_0217_raw.tif"}, {"in_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/jp2_files/DEMO998/MD662&661-F68-2017.06.06-07.39.27_MD661_2_0203_lossless.jp2", "out_fp": "/home/eodonnell/workspace/python/MouseDevTrimmed/demo_data/DEMO998/DEMO998_raw/MD662&661-F68-2017.06.06-07.39.27_MD661_2_0203_raw.tif"}]\'' &
+wait
